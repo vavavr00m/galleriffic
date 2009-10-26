5ab2de42a5e55bb1e1d0645791860e84cbd9ac72
trentfoley <trentfoley@e22ff9e4-ed5b-11dd-b00f-9fabb4b2ff13>
2009-10-26 03:41:51 +0000

    Updated home page text

--- trunk/example/js/jquery.galleriffic.js
+++ trunk/example/js/jquery.galleriffic.js
@@ -14,7 +14,7 @@
 
 	// Galleriffic static class
 	$.galleriffic = {
-		version: '2.0.0',
+		version: '2.0',
 
 		// Strips invalid characters and any leading # characters
 		normalizeHash: function(hash) {

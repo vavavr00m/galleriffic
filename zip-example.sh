83e766333b98394d89377db0869fdc9620fc3a4d
trentfoley <trentfoley@e22ff9e4-ed5b-11dd-b00f-9fabb4b2ff13>
2009-11-14 18:24:13 +0000

    Updated zip example script

--- zip-example.sh
+++ zip-example.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-zip -vr galleriffic-2.0.zip example/* -x@excludes.txt
+
+rm -rf galleriffic-2.0
+rm galleriffic-2.0.zip
+
+mkdir galleriffic-2.0
+
+cp -r example/* galleriffic-2.0
+
+zip -vr galleriffic-2.0.zip galleriffic-2.0/* -x@excludes.txt
+
+rm -rf galleriffic-2.0

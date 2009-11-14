9b7dbf870d92e55f41222f1fd89a7a636a5c522b
trentfoley <trentfoley@e22ff9e4-ed5b-11dd-b00f-9fabb4b2ff13>
2009-11-14 18:45:04 +0000

    Relocated and updated the example zip script

--- example/zip-example.sh
+++ example/zip-example.sh
@@ -5,7 +5,7 @@ rm galleriffic-2.0.zip
 
 mkdir galleriffic-2.0
 
-cp -r example/* galleriffic-2.0
+tar cvf - . | (cd galleriffic-2.0; tar xvf -)
 
 zip -vr galleriffic-2.0.zip galleriffic-2.0/* -x@excludes.txt
 
